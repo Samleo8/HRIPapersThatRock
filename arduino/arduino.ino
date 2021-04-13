7ab9d8e8e6ca6b652968650a780824b98e8bc2eb
Alex <arstrasser@gmail.com>
2021-04-12 20:01:42 -0400

    new teleop

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,26 +1,50 @@
 #define ROCK_BUTTON 2
 #define PAPER_BUTTON 3
 #define SCISSORS_BUTTON 4
+#define WIN_BUTTON 5
+#define LOSE_BUTTON 6
+#define TIE_BUTTON 7
+#define START_ROUND 8
 
 void setup() {
   pinMode(ROCK_BUTTON, INPUT_PULLUP);
   pinMode(PAPER_BUTTON, INPUT_PULLUP);
   pinMode(SCISSORS_BUTTON, INPUT_PULLUP);
+  pinMode(WIN_BUTTON, INPUT_PULLUP);
+  pinMode(LOSE_BUTTON, INPUT_PULLUP);
+  pinMode(TIE_BUTTON, INPUT_PULLUP);
+  pinMode(START_ROUND, INPUT_PULLUP);
 
   Serial.begin(9600);
+}
+
+void waitUntilLow(int pin) {
+  while (digitalRead(pin) == LOW) delay(50);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (digitalRead(ROCK_BUTTON) == LOW) {
     Serial.println("ROCK");
-    while (digitalRead(ROCK_BUTTON) == LOW) delay(10);
+    waitUntilLow(ROCK_BUTTON);
   } else if (digitalRead(PAPER_BUTTON) == LOW) {
     Serial.println("PAPER");
-    while (digitalRead(PAPER_BUTTON) == LOW) delay(10);
+    waitUntilLow(PAPER_BUTTON);
   } else if (digitalRead(SCISSORS_BUTTON) == LOW) {
     Serial.println("SCISSORS");
-    while (digitalRead(SCISSORS_BUTTON) == LOW) delay(10);
+    waitUntilLow(SCISSORS_BUTTON);
+  } else if (digitalRead(START_ROUND) == LOW) {
+    Serial.println("START");
+    waitUntilLow(START_ROUND);
+  } else if (digitalRead(WIN_BUTTON) == LOW) {
+    Serial.println("WIN");
+    waitUntilLow(WIN_BUTTON);
+  } else if (digitalRead(LOSE_BUTTON) == LOW) {
+    Serial.println("LOSE");
+    waitUntilLow(LOSE_BUTTON);
+  } else if (digitalRead(TIE_BUTTON) == LOW) {
+    Serial.println("TIE");
+    waitUntilLow(TIE_BUTTON);
   }
   
   //Wait until buttons back to normal
--- misty/misty.js
+++ misty/misty.js
@@ -1,13 +1,34 @@
 
 function playMove(move) {
   if (move == "ROCK") {
-    misty.MoveArmDegrees("both", 80, 100);
+    misty.MoveArms(80, 80, 100, 100);
+    // misty.DisplayText("ROCK")
   } else if (move == "PAPER") {
-    misty.MoveArmDegrees("both", -80, 100);
+    misty.MoveArms(-80, -80, 100, 100);
+    // misty.DisplayText("PAPER")
   } else if (move == "SCISSORS") {
-    misty.MoveArmDegrees("left", -80, 100);
-    misty.MoveArmDegrees("right", 80, 100);
+    misty.MoveArms(-80, 80, 100, 100);
+    // misty.DisplayText("SCISSORS")
   }
+  misty.UnregisterEvent("reset");
+  misty.RegisterTimerEvent("reset", 5000, false);
+}
+
+function _reset() {
+  misty.MoveArmDegrees("both", 0, 100);
+  // misty.DisplayText("")
+}
+
+function _playRock() {
+  playMove("ROCK")
+}
+
+function _playPaper() {
+  playMove("PAPER")
+}
+
+function _playScissors() {
+  playMove("SCISSORS")
 }
 
 misty.AddReturnProperty("SerialMessage", "SerialMessage");
@@ -19,7 +40,47 @@ function _SerialMessage(data) {
       if(data !== undefined && data !== null) {
           var move = data.AdditionalResults[0].Message;
           misty.Debug(move);
-          playMove(move)
+          if (move == "START") {
+            misty.PlayAudio("start.mp3");
+            r = Math.random()*3;
+            move = r < 1? "ROCK":(r<2?"PAPER":"SCISSORS")
+            if (r < 1) {
+              misty.RegisterTimerEvent("playRock", 2000, false);
+            } else if (r < 2) {
+              misty.RegisterTimerEvent("playPaper", 2000, false);
+            } else {
+              misty.RegisterTimerEvent("playScissors", 2000, false);
+            }
+          } else if (move == "ROCK" || move == "PAPER" || move == "SCISSORS") {
+            var cheat = "FAVOR_MISTY"
+            if (cheat == "FAVOR_MISTY") {
+              if (move == "ROCK") {
+                playMove("PAPER")
+              } else if (move == "PAPER") {
+                playMove("SCISSORS")
+              } else if (move == "SCISSORS") {
+                playMove("ROCK")
+              }
+            }
+            else if (cheat == "FAVOR_HUMAN") {
+              if (move == "ROCK") {
+                playMove("SCISSORS")
+              } else if (move == "PAPER") {
+                playMove("ROCK")
+              } else if (move == "SCISSORS") {
+                playMove("PAPER")
+              }
+            }
+            else {
+              playMove(move)
+            }
+          } else if (move == "WIN") {
+            misty.PlayAudio("win.mp3");
+          } else if (move == "LOSE") {
+            misty.PlayAudio("lose.mp3");
+          } else if (move == "TIE") {
+            misty.PlayAudio("tie.mp3");
+          }
       }
   }
   catch(exception) {
