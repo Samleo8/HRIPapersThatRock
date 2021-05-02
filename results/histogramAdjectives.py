65c43ec5d4c7e4f08640db60b2eb2b86664b2a08
Samuel Leong <leowhiz@yahoo.com.sg>
2021-05-02 15:54:38 -0400

    Have a sorted output

--- results/histogramAdjectives.py
+++ results/histogramAdjectives.py
@@ -1,16 +1,17 @@
 import numpy as np
 
+histogram = {}
+
 with open("adjectives.txt", "r") as f:
     wordList = f.readline().strip().split(", ")
-
-    histogram = {}
 
     for word in wordList:
         word = word.lower().strip()
         histogram[word] = histogram.get(word, 0) + 1
 
-    print(histogram)
+histogramSorted = sorted(histogram.items(), key=lambda kv: (kv[1], kv[0]))
+histogramSorted.reverse()
 
 with open("output.csv", "w") as f:
-    for word in histogram:
-        print(f"{word},{histogram[word]}", file=f)
+    for word, count in histogramSorted:
+        print(f"{word},{count}", file=f)
